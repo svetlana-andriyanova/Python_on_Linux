38e23307bc4a7cfc2b413fb7f8a486296aa44ed9
svetlana-andriyanova <nostress58@yandex.ru>
2023-11-13 19:57:45 +0300

    solution for task hw1 fin

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+'''
+Написать функцию на Python, которой передаются в качестве параметров команда и текст.
+Функция должна возвращать True, если команда успешно выполнена и текст найден в её выводе и False в противном случае.
+Передаваться должна только одна строка, разбиение вывода использовать не нужно.
+'''
+
+
 import subprocess
 
-def execute_command(cmd, text):
+def check_command(cmd, text):
     result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
     if result.returncode == 0 and text in result.stdout:
         return True
@@ -8,6 +15,6 @@ def execute_command(cmd, text):
         return False
 
 if __name__ == '__main__':
-    print(execute_command('ls /home/user', 'Видео'))
-    print(execute_command('ls --help', 'Выдаёт информацию о ФАЙЛАХ'))
-    print(execute_command('cat /etc/os-release', 'Страница'))
+    print(check_command('ls /home/user', 'Видео'))
+    print(check_command('ls --help', 'Выдаёт информацию о ФАЙЛАХ'))
+    print(check_command('cat /etc/os-release', 'Страница'))
